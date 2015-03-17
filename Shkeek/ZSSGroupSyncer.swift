d46ab12f64f3ff746e727af91b9403937c5804ec
shakked <zshakked@stevens.edu>
2015-03-16 21:58:44 -0400

    Added some new files and set up the foundation for the CoreData store

---
+++ Shkeek/ZSSGroupSyncer.swift
@@ -0,0 +1,13 @@
+//
+//  ZSSGroupSyncer.swift
+//  Shkeek
+//
+//  Created by Zachary Shakked on 3/16/15.
+//  Copyright (c) 2015 Shkeek Inc. All rights reserved.
+//
+
+import UIKit
+
+class ZSSGroupSyncer: NSObject {
+   
+}
---
+++ Shkeek/ZSSLocalStore.swift
@@ -0,0 +1,13 @@
+//
+//  ZSSLocalStore.swift
+//  Shkeek
+//
+//  Created by Zachary Shakked on 3/16/15.
+//  Copyright (c) 2015 Shkeek Inc. All rights reserved.
+//
+
+import UIKit
+
+class ZSSLocalStore: NSObject {
+   
+}
